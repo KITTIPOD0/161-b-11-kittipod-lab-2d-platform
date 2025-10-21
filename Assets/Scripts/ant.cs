b030100140a328dbee7a081ade083696a22cfa6d
KITTIPOD0 <kittipod.sinr@bumail.net>
2025-10-21 15:55:12 +0700

    antmovebutdown

--- Assets/Scripts/ant.cs
+++ Assets/Scripts/ant.cs
@@ -7,14 +7,24 @@ public class ant : Enemy
     public Transform[] MovePoint; 
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+    private void FixedUpdate()
+    {
+        Behavior();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        base.Intialize(100);
+        base.Intialize(20);
+         
+       DamageHit = 20;
+
+        velocity = new Vector2(-1.0f,0.0f);
+
     }
+   
 
     // Update is called once per frame
     void Update()
